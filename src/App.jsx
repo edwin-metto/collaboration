f2fe6881129c671fa140b3ae236557cb47d60391
WillSketcher254 <wilmuch001@gmail.com>
2024-10-31 11:32:08 +0300

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
   return (
     <>
       <div>
-      <h1>collaborators</h1>
-      <h2>Maina kamau</h2>
-        
+        <h1>collaborators</h1>
+        <h2>Maina kamau</h2>
+
       </div>
-    
+
     </>
   )
 }
