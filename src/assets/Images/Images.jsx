85e1bd6c888c362a433b83370ed6946c4f71eb43
eugen179 <maisheugene@gmail.com>
2024-11-04 13:21:59 +0300

    chao

--- src/assets/Images/Images.jsx
+++ src/assets/Images/Images.jsx
@@ -1,9 +1,15 @@
 import Salad1 from './SausagePotatoSalad.jpg'
 import Salad2 from './ChipotleChickenSalad.jpg'
 import Salad3 from './TomatoCucumberSalad.jpg'
+import Salad4 from  './ChickenSalad.jpg'
+import Salad5 from  './MangoBlisssalad.jpg'
+import Salad6 from  './AvocadoTunasalad.jpg'
 
 export const images = {
     Salad1,
     Salad2,
-    Salad3
+    Salad3,
+    Salad4,
+    Salad5,
+    Salad6
 }
