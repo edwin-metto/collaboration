2532f07f9e59f1d83986146a70f4c19116e58767
eugen179 <maisheugene@gmail.com>
2024-11-04 13:29:52 +0300

    zi

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <>
-      <div>
-      <h1 className='text-xl-font bold text-red-500'>collaborators</h1>
+      <div className='bg-green-800'>
+      <h1 className='bg-blue-500 text-xl-font bold text-red-500'>collaborators</h1>
       <h2>  Eugene maina kamau</h2>
       <Food/>
         
