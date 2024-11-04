ad366f720848fcd470557e5012b302b7e4b34042
Edwin-metto <edwinmetto89@gmail.com>
2024-11-04 12:04:24 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import FoodOrder from '.Components/FoodOrder'
+
 function App() {
 
 
@@ -11,6 +13,8 @@ function App() {
       <div>
       <h1>collaborators</h1>
       <h2>  Eugene maina </h2>
+      
+
         
       </div>
     
