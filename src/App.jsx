dabd625758724843af2821f514be4fd7df9fc2dd
eugen179 <maisheugene@gmail.com>
2024-11-04 13:20:04 +0300

    hey

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Food from './assets/Images/Food'
 
 function App() {
 
@@ -9,8 +10,9 @@ function App() {
   return (
     <>
       <div>
-      <h1>collaborators</h1>
+      <h1 className='text-xl-font bold text-red-500'>collaborators</h1>
       <h2>  Eugene maina </h2>
+      <Food/>
         
       </div>
     
