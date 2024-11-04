41d5412514e470fe6df4e33f62d9e63a131d67c7
WillSketcher254 <wilmuch001@gmail.com>
2024-11-04 10:58:33 +0300

    pull tryal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
-import Food from './assets/Images/Food'
-
+import FoodOrder from './Payment/FoodOrder'
 function App() {
   return (
     <>
       <div>
+        <FoodOrder />
 
-        <Food />
 
       </div>
     </>
