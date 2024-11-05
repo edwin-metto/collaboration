d28b0f466f66addaee0e62e2fdcd45418d1352ac
WillSketcher254 <wilmuch001@gmail.com>
2024-11-05 11:13:11 +0300

    order changes

--- src/assets/Images/Food.jsx
+++ src/assets/Images/Food.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { images } from './Images';
 function Food() {
+    const[orderIndex, setOrderIndex] = useState([])
 
     const Food = [
       
@@ -220,28 +221,32 @@ function Food() {
         rating: '4'
       }
       
-      
-
-
-
-
     ];
+    const toogleOrder = (index) =>{
+      setOrderIndex((prevIndex)=>
+      prevIndex.includes(index) ? prevIndex.filter((i)=> i !== index)
+    : [...prevIndex,index]);
+    }
   
     return (
         <div>
             <div>
                 <ul className='flex flex-wrap  text-black'>
                     {Food.map((meal, index) => (
-                        <div key={index} className='rounded-2xl bg-gray-200 m-5 overflow-hidden text-center max-w-60 shadow-lg transition ease-out delay-160 hover:scale-110 '>
+                        <div key={index} className='rounded-2xl bg-gray-200 m-5 overflow-hidden text-center max-w-60 shadow-lg transition ease-out delay-160 hover:scale-110 '
+                        onClick={()=>toogleOrder(index)}
+                        >
                             <li><img src={meal.image} alt="" className='h-60 w-60 object-cover'/></li>
                             <li className='text-[20px]'>{meal.name}</li>
                             <li> {meal.description}</li>
                             <p className='text-orange-500 text-[20px]'>$ {meal.price}</p>
-                            <li className='text-orange-500'>Rated, {meal.rating} &#9733; </li>
+                            <li className='text-orange-500 flex px-3 justify-between'>
+                              <div>Rated, {meal.rating} &#9733; </div>
+                              <div className={orderIndex.includes(index) ? 'text-white bg-green-500 px-2': ''}>{`${orderIndex.includes(index) ? 'Order Placed':''}`}</div>
+                            </li>
                         </div>
                     ))}
                 </ul>
-
             </div>
             <div>
               <button >Place Order</button>
