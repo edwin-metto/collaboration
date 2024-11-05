69a6fc257ac69e7479027d3b68654e65c8f281ed
WillSketcher254 <wilmuch001@gmail.com>
2024-11-05 13:48:44 +0300

    menu update

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { images } from '../assets/Images/Images'
+
+function Menu() {
+    return (
+        <div className='text-black p-5'>
+            <div className='flex justify-center p-3'>
+                <p className='font-bold text-[20px]'>MENU</p>
+            </div>
+            <div className='flex justify-center p-3 text-[18px]'>
+                <p className=''>Select a meal of choice from the choices below </p>
+            </div>
+            
+
+            <div className='flex justify-between px-20 font-bold text-orange-600'>
+                <div className='hover:scale-110 hover:text-green-600 transition ease-out delay-150'>
+                    <p>Cakes</p>
+                    <img src={images.CakeLogo} className='h-20 w-20 object-cover border-[4px] rounded-full hover:border-green-500 m-2' />
+                </div>
+                <div className='hover:scale-110 hover:text-green-600 transition ease-out delay-150'>
+                    <p>Salad</p>
+                    <img src={images.SaladLogo} className='h-20 w-20 object-cover border-[4px] rounded-full hover:border-orange-500 m-2' />
+
+                </div>
+                <div className='hover:scale-110 hover:text-green-600 transition ease-out delay-150'>
+                    <p>Rolls</p>
+                    <img src={images.RollsLogo} className='h-20 w-20 object-cover border-[4px] rounded-full hover:border-green-500 m-2' />
+
+                </div>
+                <div className='hover:scale-110 hover:text-green-600 transition ease-out delay-150'>
+                    <p>Dessert</p>
+                    <img src={images.DessertLogo} className='h-20 w-20 object-cover border-[4px] rounded-full hover:border-orange-500 m-2' />
+           
+                </div>
+                <div className='hover:scale-110 hover:text-green-600 transition ease-out delay-150'>
+                    <p>Noodle</p>
+                    <img src={images.NoodleLogo} className='h-20 w-20 object-cover border-[4px] rounded-full hover:border-green-500 m-2' />
+
+                </div>
+                <div className='hover:scale-110 hover:text-green-600 transition ease-out delay-150'>
+                    <p>Vegetable</p>
+                    <img src={images.VegyLogo} className='h-20 w-20 object-cover border-[4px] rounded-full hover:border-orange-500 m-2' />
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Menu
