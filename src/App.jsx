9540123c313aa6db331fb82f4973528cc74e3531
Edwin-metto <edwinmetto89@gmail.com>
2024-11-05 18:04:12 +0300

    upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
+
 import './App.css'
 import Food from './assets/Images/Food'
-<<<<<<< HEAD
 import Login from './Components/Login'
-=======
 import Home from './Components/Home'
->>>>>>> d28b0f466f66addaee0e62e2fdcd45418d1352ac
 
 import FoodOrder from './Payment/FoodOrder'
 
@@ -13,16 +11,17 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className=''>
+    
+        
         <Home/>
         <Food/>
-<<<<<<< HEAD
+        <FoodOrder/>
         <Login/>
         
+        
+        
 
-=======
-        <FoodOrder/>
->>>>>>> d28b0f466f66addaee0e62e2fdcd45418d1352ac
       </div>
 
     </>
