39a5458ea551b30ec80df03724e74d6d73612c7e
WillSketcher254 <wilmuch001@gmail.com>
2024-11-06 21:45:47 +0300

    new commit

--- src/Payment/Location.jsx
+++ src/Payment/Location.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Location = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
+    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }} className='w-full flex flex-wrap'>
       <div style={{ width: '60%' }}>
         <h2>Delivery Information</h2>
         <form style={{ display: 'grid', gap: '10px' }}>
           <div style={{ display: 'flex', gap: '10px' }}>
-            <input type="text" placeholder="First name" style={{ flex: 1 }} />
-            <input type="text" placeholder="Last name" style={{ flex: 1 }} />
+            <input type="text" placeholder="First name" style={{ flex: 1 }}  className='p-1 px-5 rounded-xl m-2 bg-white text-black '/>
+            <input type="text" placeholder="Last name" style={{ flex: 1 }}  className='p-1 px-5 rounded-xl m-2 bg-white text-black '/>
           </div>
-          <input type="email" placeholder="Email address" />
-          <input type="text" placeholder="Street" />
+          <input type="email" placeholder="Email address"  className='p-1 px-5 rounded-xl m-2 bg-white text-black '/>
+          <input type="text" placeholder="Street"  className='p-1 px-5 rounded-xl m-2 bg-white text-black '/>
           <div style={{ display: 'flex', gap: '10px' }}>
-            <input type="text" placeholder="City" style={{ flex: 1 }} />
-            <input type="text" placeholder="State" style={{ flex: 1 }} />
+            <input type="text" placeholder="City" style={{ flex: 1 }}  className='p-1 px-5 rounded-xl m-2 bg-white text-black '/>
+            <input type="text" placeholder="State" style={{ flex: 1 }}  className='p-1 px-5 rounded-xl m-2 bg-white text-black '/>
           </div>
-          <input type="text" placeholder="Phone" />
+          <input type="text" placeholder="Phone"  className='p-1 px-5 rounded-xl m-2 bg-white text-black '/>
         </form>
       </div>
 
@@ -38,7 +38,7 @@ const Location = () => {
             </tr>
           </tbody>
         </table>
-        <button style={{ padding: '10px 20px', backgroundColor: '#FF6A3D', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+        <button className=' w-[200px] p-1 border-none rounded-l bg-blue-500 hover:bg-blue-600 hover:text-white'>
           Proceed To Payment
         </button>
       </div>
