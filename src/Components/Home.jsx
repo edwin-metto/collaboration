61c341f577682da5107049a2291c2f30e7805b2e
WillSketcher254 <wilmuch001@gmail.com>
2024-11-06 12:01:00 +0300

    banner change 3

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,19 +5,21 @@ import Banner from '../assets/Images/Banner'
 
 
 function Home() {
-    const [menuOn,setMenuOn] = useState(false)
+    const [menuOn,setMenuOn] = useState(null)
     return (
         <div>
             <div className='flex justify-between px-10 bg-gray-200'>
                 <h1 className='text-orange-400'>Pot-Store</h1>
                 <div>
                     <button className='border-none' onClick={()=>setMenuOn(false)}>Home</button>
-                    <button className='border-none' onClick={()=>setMenuOn(!menuOn)}>Menu</button>
+                    <button className='border-none' onClick={()=>setMenuOn(true)}>Menu</button>
                 </div>
             </div>
             <div>
-                <Banner/>   
-                {menuOn ? <Menu/> : <Food />}
+                {menuOn === null && <Banner/>}
+                {menuOn === true && <Menu/>}
+                {menuOn === false && <Food />}
+                
                 
             </div>
         </div>
