7f16faf80e1737983e366d2b92d1bf615d1d4a62
WillSketcher254 <wilmuch001@gmail.com>
2024-11-07 13:06:49 +0300

    push

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -27,8 +27,8 @@ function Home() {
                     {!showOrder ? ( <div className='sticky top-0 z-99'>
                         
                     {menuOn === true ? (<div className=' bg-gray-200'><Menu /></div>) : ( <div><Food /></div> )}
-                    </div> ) : ''} 
-                    
+                    </div> ) : ''}
+                    <Food />
                     {menuOn === null ? (
                         <button className='border-none absolute bottom-0' onClick={() => setMenuOn(false)}>Visit our Home Page</button>) : ''}
                 </div>
